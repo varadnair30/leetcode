47bc27fc9b9cfa4c688bdff7d07a826a0959f3ff
Varad Nair <52270722+varadnair30@users.noreply.github.com>
2025-04-01 23:21:13 -0500

    Time: 0 ms (100%), Space: 17.9 MB (66.49%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -2,13 +2,13 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
 
+        #Using Bit Manipulation where shifting of significant bit can decide where the no. is odd or even
         
-        
-        for i in nums:
-            if nums.count(i)%2 == 1:
-                return False
-            
-        return True
+        bit_mask = 0  
+        for num in nums:
+            bit_mask ^= 1 << num  
+
+        return bit_mask == 0
 
             
         
