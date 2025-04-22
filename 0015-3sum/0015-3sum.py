051c9910b0e1b340e487798f0cd2626b2e83b149
Varad Nair <52270722+varadnair30@users.noreply.github.com>
2025-04-22 02:29:59 -0500

    Time: 598 ms (46.06%), Space: 20.9 MB (23.16%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,36 +1,36 @@
 class Solution:
-    def threeSum(self, arr: List[int]) -> List[List[int]]:
-        n=len(arr)
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
         res=[]
-        arr.sort()
-        
+
+        nums.sort()
+        n=len(nums)
         for i in range(n):
-            # remove the duplicates
-            
-            if i!=0 and arr[i]==arr[i-1]:
+
+            if (i>0 and nums[i]==nums[i-1]):
                 continue
+
             j=i+1
             k=n-1
-            
-            while j<k:
-                total_sum=arr[i]+arr[j]+arr[k]
-                if total_sum<0:
+
+            while (j<k):
+                sum=nums[i]+nums[j]+nums[k]
+                if(sum<0):
+
                     j+=1
-                elif total_sum>0:
+                    
+
+                elif(sum>0):
                     k-=1
                 else:
-                    temp=[arr[i],arr[j],arr[k]]
-                    res.append(temp)
+                    res.append([nums[i],nums[j],nums[k]])
                     j+=1
                     k-=1
-                    while j<k and arr[j]==arr[j-1]:   #skip the duplicates
+                    while (j<k and nums[j]==nums[j-1]):
                         j+=1
-                    while j<k and arr[k]==arr[k+1]:   #skip the duplicates
-                        k-=1
-                    
-        return res    
-                        
-                        
-        
-        
+                    while (j<k and nums[k]==nums[k+1]):
+                        k-=1 
+
+        return res
+
         
