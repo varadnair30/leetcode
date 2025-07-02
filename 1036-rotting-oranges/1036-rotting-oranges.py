4595f0432c306e93deadcaf61507dbbe34cb5898
Varad Nair <52270722+varadnair30@users.noreply.github.com>
2025-07-02 00:05:53 -0500

    Time: 4 ms (48.14%), Space: 17.8 MB (44.49%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -2,67 +2,52 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
 
+        if not grid: return 0
+
+        # Do bfs traversal if current orange is rotten (2) , then explore in nearby directions and see if there is fresh orange(1), store them in queue and convert them to 2. Everytime done with this process, do iteration++ variable
+        # do not need set because we are making changes in the grid itself
+        # store directions
+        
+        ROWS,COLS= len(grid),len(grid[0])
+
+        
         q=deque()
         time=fresh=0
-        ROWS,COLS = len(grid),len(grid[0])
-
+        
         for r in range(ROWS):
             for c in range(COLS):
 
-                if grid[r][c]== 1:
+                if grid[r][c]==1:
                     fresh+=1
-
-                if grid[r][c] ==2:
-                    q.append([r,c])
-
+            
+                if grid[r][c]==2:
+                    q.append((r,c))
+        
         directions=[[1,0],[-1,0],[0,1],[0,-1]]
 
         while q and fresh>0:
-            
             for i in range(len(q)):
-                r,c = q.popleft()
+                r,c=q.popleft()
 
                 for dr,dc in directions:
+                    nr,nc = dr+r,dc+c
 
-                    nr, nc = dr+r, dc+c
+                    if 0<=nr<ROWS and 0<=nc<COLS and grid[nr][nc]==1:
+                        
 
-                    if nr<0 or nr>=ROWS or nc<0 or nc>=COLS or grid[nr][nc]!=1: # either out of bounds or if grid cell is not a fresh orange
+                        grid[nr][nc]=2
 
-                        continue
+                        q.append((nr,nc))
 
-                    grid[nr][nc]= 2
-
-                    q.append([nr,nc])
-
-                    fresh-=1
+                        fresh-=1
             time+=1
-
         return time if fresh==0 else -1
 
+        
             
 
 
 
 
 
-        
-
-
-        
-
-
-
-
-            
-
-
-        # If near '2' , 1s will start rotting 
-        # each 1st near 2 will rot in each different iterations
-
-        # return the minimum possible iterations
-
-
-
-
-        
         
