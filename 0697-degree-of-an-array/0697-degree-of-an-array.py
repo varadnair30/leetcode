dad00ca6bfd0082cadd928b74bd9f562e30041f7
Varad Nair <52270722+varadnair30@users.noreply.github.com>
2025-08-06 23:57:53 -0500

    Time: 18 ms (70.88%), Space: 19.6 MB (44.7%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -7,13 +7,13 @@ class Solution:
 
         if (len(nums)>50000 and len(nums)<1): return 0
 
-        count, first = defaultdict(list), {}
+        count, first = defaultdict(int), {}
         degree, ans = 0, len(nums)
 
         for i, num in enumerate(nums):
             if num not in first:
                 first[num] = i
-            count[num] = count.get(num, 0) + 1
+            count[num] = count[num] + 1
 
             if count[num] > degree:
                 degree = count[num]
