7010f6a502398a0d255af242765df1009e4ca75f
Varad Nair <52270722+varadnair30@users.noreply.github.com>
2025-09-06 10:49:06 -0500

    Time: 123 ms (53.5%), Space: 18.2 MB (23.88%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        n=len(s)
+        count=defaultdict(int)
+
+        maxf=l=res=r=0
+
+        while r<n:
+
+            count[s[r]]+=1
+
+            maxf=max(maxf,count[s[r]])
+
+            while((r-l+1)-maxf>k):
+
+                count[s[l]]-=1
+                l+=1
+
+            res=max(res,r-l+1)
+            r+=1
+
+        return res
+        
