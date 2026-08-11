24d36edbd99cf2f4e3515babda9dfebb7d738e0d
Varad Nair <52270722+varadnair30@users.noreply.github.com>
2026-08-11 12:49:19 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,18 +1,24 @@
 from collections import defaultdict
 class Solution:
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        
-        
+    def subarraySum(self, arr: List[int], k: int) -> int:
+
+        n=len(arr)
+        preSum=cnt=0
         mpp=defaultdict(int)
         mpp[0]=1
-        preSum,cnt=0,0
-        n=len(nums)
+
         for i in range(n):
-            preSum+=nums[i]
-            remove=preSum-k
-            cnt+=mpp[remove]
-            
-            mpp[preSum]+=1
-            
+
+            preSum+=arr[i]
+
+            rem=preSum-k
+            cnt= cnt+mpp[rem]
+
+            mpp[preSum]=1
+
         return cnt
+
+        
+
+        
         
