7a68ac057fa8aaa063b69e1ba29be0df19df512a
Varad Nair <52270722+varadnair30@users.noreply.github.com>
2026-08-18 19:44:33 -0500

    Time: 5 ms (75.38%), Space: 23.7 MB (25.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,46 +1,42 @@
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums: return []
+        res=[]
+        counter=Counter(nums)
+
+        for num, count in counter.items():
+
+            if count> (len(nums) // 3):
+                res.append(num)
+
+        return res
+
         
+'''
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+
+        # Dictionary to store the frequency of each element
+        counts = {}
+        for num in nums:
+            counts[num] = counts.get(num, 0) + 1
+
+        res = []
+        n = len(nums)
+        for num, count in counts.items():
+            if count > (n // 3):
+                res.append(num)
+
+        return res
+
+    '''
+
         
-        cnt1,cnt2=0,0
-        el1=float('-inf')
-        el2=float('-inf')
-        n=len(nums)
-        for i in range(n):
-            if (cnt1==0 and el2!=nums[i]):
-                cnt1=1
-                el1=nums[i]
-            elif(cnt2==0 and el1!=nums[i]):
-                cnt2=1
-                el2=nums[i]
-            elif(nums[i]==el1):
-                cnt1+=1
-            elif(nums[i]==el2):
-                cnt2+=1
-            else:
-                cnt1-=1
-                cnt2-=1
-                
-        ls=[]
-        cnt1,cnt2=0,0
-        
-        for i in range(n):
-            if(el1==nums[i]):
-                cnt1+=1
-            if(el2==nums[i]):
-                cnt2+=1
-                
-        mini=(n//3) + 1
-        if(cnt1>=mini):
-            ls.append(el1)
-        if(cnt2>=mini):
-            ls.append(el2)
-        ls.sort()
-        return ls
-        
-        
-        
-        
-        
-        
+
+
+
+
         
